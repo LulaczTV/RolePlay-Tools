974708e325dcb0a8d77e426ed0bcb4cbe97a8b18
pan_andrzej <98231161+LulaczTV@users.noreply.github.com>
2024-01-14 22:08:54 +0100

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,17 +1,21 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using RolePlay_Tools;
 
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-
+#if EXILED
 [assembly: AssemblyTitle("RolePlay-Tools")]
+#else
+[assembly: AssemblyTitle("RolePlay-Tools-NW")]
+#endif
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("pan_andrzej")]
 [assembly: AssemblyProduct("RolePlay-Tools")]
-[assembly: AssemblyCopyright("Copyright ©pan_andrzej  2023")]
+[assembly: AssemblyCopyright("Copyright ©pan-andrzej.xyz  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +37,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Plugin.PluginVersion)]
+[assembly: AssemblyFileVersion(Plugin.PluginVersion)]

82e1713b450b8dd01c882d3daf9328ac3f793f0b
kuba kasprowski <kubakasprowski0604@gmail.com>
2024-05-01 20:44:35 +0200

    enum fix????

--- Enums/CommandType.cs
+++ Enums/CommandType.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,25 +18,3 @@ namespace RolePlay_Tools.Enums
         Try = 7,
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RolePlay_Tools.Enums
-{
-    public enum CommandType
-    {
-        None = 0,
-        Do = 1,
-        Me = 2,
-        Ooc = 3,
-        Push = 4,
-        Steal = 5,
-        Title = 6,
-        Try = 7,
-    }
-}
->>>>>>> 37f8272ee0ea6759cd712e8b2416793da2451c05
